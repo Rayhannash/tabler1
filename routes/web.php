80477324faf8ae3232f5c06c9ed779bdcdfde7a9
zakiusamah7 <zakiusamah7@gmail.com>
2025-06-18 11:27:01 +0700

    Pesan commit kamu

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 
 use Illuminate\Support\Facades\Route;
 
@@ -25,7 +25,6 @@ Route::middleware(['auth', 'check-access', 'authorize-access'])->group(function 
     Route::resource('menu', App\Http\Controllers\MenuController::class);
     
     Route::get('test', [App\Http\Controllers\UserExtrasController::class, 'index'])->name('test');
-    
 
 
     
